07de8bcb885764ade4d061e077602e8fbed1bfaf
jackfalcone <yves.daetwyler@gmail.com>
2020-07-12 21:01:09 +0200

    maybe?

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use('/static', express.static('public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Node app is working!');
 });
