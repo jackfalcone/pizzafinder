f7323d7ef752a9b873e606e98c6a1f8db7048eac
jackfalcone <yves.daetwyler@gmail.com>
2020-07-12 03:54:13 +0200

    Finally?!

--- Server.js
+++ Server.js
@@ -1,16 +1,12 @@
-const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require('express');
 const app = express();
-app.use(favicon('./public' + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static('./public'));
-app.use(express.static(path.join('./public', 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join('./public', 'build', 'index.html'));
+app.listen(port, () => {
+   console.log('Server is up!');
 });
-app.listen(port);
