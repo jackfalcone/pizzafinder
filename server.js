9bcc87d1475192c6712d2f41837516d96f9fee93
jackfalcone <yves.daetwyler@gmail.com>
2020-07-12 15:21:17 +0200

    maybe?

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3000;
+var express = require('express');
+var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
